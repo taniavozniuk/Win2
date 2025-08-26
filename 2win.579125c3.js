267319ce2652ac6227ac47a63a6d3ac2297c8a39
Tetiana Vozniuk <taniavoznyk19655@gmail.com>
2025-08-26 16:40:45 +0300

    Updates

---
+++ 2win.579125c3.js
@@ -0,0 +1,3 @@
+console.log("Parcel \u043F\u0440\u0430\u0446\u044E\u0454!");
+
+//# sourceMappingURL=2win.579125c3.js.map
---
+++ 2win.c42e4c4a.js
@@ -0,0 +1,2 @@
+console.log("Parcel працює!");
+//# sourceMappingURL=2win.c42e4c4a.js.map
